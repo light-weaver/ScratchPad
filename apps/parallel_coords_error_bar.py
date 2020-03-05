25476d20e79e021efd50c8e1eeef74c34fc23b04
Bhupinder Saini <bhupinder.saini2.71@gmail.com>
2020-03-05 16:46:06 +0200

    Cool parallel coordinates

--- apps/parallel_coords_error_bar.py
+++ apps/parallel_coords_error_bar.py
@@ -1,16 +1,19 @@
 import dash
 import plotly.graph_objects as go
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+import json
 import numpy as np
+from functools import reduce
+from dash.exceptions import PreventUpdate
 
-names = ["x1", "x2", "x3"]  # Objective names
+names = ["x1", "x2", "x3","x4","x5"]  # Objective names
 stdnames = [name + "_std" for name in names]
-means = pd.DataFrame(np.random.rand(10, 3), columns=names)  # Mean values
-std = pd.DataFrame(np.random.rand(10, 3) * 0.5, columns=stdnames)  # Standard deviation
+means = pd.DataFrame(np.random.rand(10, 5), columns=names)  # Mean values
+std = pd.DataFrame(np.random.rand(10, 5) * 0.1, columns=stdnames)  # Standard deviation
 
 df = means.join(std)
 
@@ -18,6 +21,13 @@ df = means.join(std)
 app = dash.Dash(__name__)
 app.layout = html.Div(
     [
+        html.Div(
+            id="datatable-interactivity-container",
+            children=[
+                html.Div(id="brushes", children=[[]], hidden=True),
+                dcc.Graph(id="par_coords", config={}),
+            ],
+        ),
         dash_table.DataTable(
             id="datatable-interactivity",
             columns=[
@@ -38,7 +48,6 @@ app.layout = html.Div(
             page_current=0,
             page_size=10,
         ),
-        html.Div(id="datatable-interactivity-container"),
     ]
 )
 
@@ -54,8 +63,8 @@ def update_styles(selected_columns):
     ]
 
 
-@app.callback(
-    Output("datatable-interactivity-container", "children"),
+"""@app.callback(
+    Output("par_coords", "figure"),
     [
         Input("datatable-interactivity", "derived_virtual_data"),
         Input("datatable-interactivity", "derived_virtual_selected_rows"),
@@ -97,7 +106,49 @@ def update_graphs(rows, derived_virtual_selected_rows):
             opacity=opacity[i],
         )
 
-    return [dcc.Graph(id="plot", figure=fig)]
+    return fig
+"""
+
+
+@app.callback(
+    [Output("par_coords", "figure"), Output("brushes", "children")],
+    [Input("par_coords", "selectedData")],
+    [State("brushes", "children"), State("par_coords", "figure")],
+)
+def brush_plot(selectedData, brushes, fig):
+    if selectedData is None or fig is None:
+        fig = go.Figure(layout={"dragmode": "select"})
+        for i in range(len(df)):
+            fig.add_scatter(
+                x=list(range(int(df.shape[1]/2))),
+                y=df[names].loc[i].values,
+                error_y=dict(
+                    type="data",  # value of error bar given in data coordinates
+                    array=df[stdnames].loc[i].values,
+                    visible=True,
+                ),
+                line=dict(color="red"), 
+                opacity=1,
+            )
+        return [fig, brushes]
+    else:
+        # print(selectedData["range"])
+        print(selectedData["points"])
+        if not selectedData["points"]:
+            raise PreventUpdate
+        selected_indices = [dicts["curveNumber"] for dicts in selectedData["points"]]
+        if not brushes[-1]:
+            current_brush = list(range(len(fig["data"])))
+            current_brush = [current_brush] * len(fig["data"][0]["x"])
+            brushes = [current_brush]
+        current_key = selectedData["points"][0]["pointNumber"]
+        brushes[-1][current_key] = selected_indices
+        selected_indices = reduce(lambda x, y: set(x) & set(y), brushes[-1])
+        for index in range(len(fig["data"])):
+            fig["data"][index]["line"]["color"] = (
+                "red" if index in selected_indices else "grey"
+            )
+        return [fig, brushes]
 
 
 if __name__ == "__main__":
